418185f3e6e21f2e315047b6ee070575ff45f330
Dominik Klauser <klauser.dominik@aon.at>
2022-01-15 21:53:17 +0100

    dal

--- app/src/main/java/at/fhooe/smaproject/ui/RecipeOverviewActivity.java
+++ app/src/main/java/at/fhooe/smaproject/ui/RecipeOverviewActivity.java
@@ -42,6 +42,37 @@ public class RecipeOverviewActivity extends AppCompatActivity {
         filterIcon.setOnClickListener(v -> {
             toggleFilters();
         });
+
+        // great dao tests ((:
+        /*CategoryDao dao = new CategoryDao(this);
+        dao.findAll();
+        Log.d("dbtest", dao.findById(2).getName());*/
+
+        /*DescriptionImageDao dao = new DescriptionImageDao(this);
+        Log.d("dbtest", dao.findById(1).getImagePath());
+        dao.insert(new DbDescriptionImage(-1, "xPath", 2, 1));
+        Log.d("dbtest", dao.findById(2).getImagePath());
+        dao.delete(2);
+        if(dao.findById(2) == null) Log.d("dbtest", "juhuuui");*/
+
+        /*RecipeDao dao = new RecipeDao(this);
+        Log.d("dbtest", dao.findById(1).getTitle());
+        DbRecipe newRecipe = new DbRecipe(2, "xTitle", null,
+                "xDescription", "xComment", 3);
+        dao.insert(newRecipe);
+        Log.d("dbtest", ""+dao.findById(2).getRating());
+        newRecipe.setRating(4);
+        dao.update(newRecipe);
+        Log.d("dbtest", ""+dao.findById(2).getRating());
+        dao.delete(2);
+        if(dao.findById(2) == null) Log.d("dbtest", "juhuuui");*/
+
+        /*RecipeCategoryDao dao = new RecipeCategoryDao(this);
+        Log.d("dbtest", ""+dao.findByRecipeIdAndCategoryId(1,1).getRecipeId());
+        dao.insert(new DbRecipeCategory(1,2));
+        Log.d("dbtest", ""+dao.findByCategoryId(2).iterator().next().getRecipeId());
+        dao.delete(1, 2);
+        if(dao.findByCategoryId(2).size() == 0) Log.d("dbtest", "juhuuui");*/
     }
 
 
